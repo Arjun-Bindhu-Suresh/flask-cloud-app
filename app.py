645463730dedd60c1a8aa633b234bd324189c95f
Arjun Bindhu Suresh <arjunbsuresh@gmail.com>
2025-03-29 20:00:05 -0400

    Test Cloud Build trigger

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return jsonify({'message': 'Hello Cloud!'})
+    return jsonify({'message': '🚀 Hello from Cloud Build!'})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
